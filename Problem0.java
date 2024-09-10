4018825f5bbcea6683fb5b6ffb85452c321cd82c
PhillipTrinh <pdtrinh100@student.hbuhsd.edu>
2024-09-10 11:19:00 -0700

    g

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,10 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double = mAMA= 3;
+    System.out.println(mAMA);
+
+    int other =3.8
+    System.out.println(other);
   }
 }
