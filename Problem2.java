a039afc4860a97e3cec164e907b5b6af75bf4576
PhillipTrinh <pdtrinh100@student.hbuhsd.edu>
2024-09-12 10:51:21 -0700

    lololoooloolooolollooollolo

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,19 @@
+public class Problem0
+{
+  public static void main(String[] args)
+  {
+    int number = 123;
+    int digit1 = (number % 10); // print 3
+    number = number / 10;  //make number 12
+
+    int digit2 = (number % 10); // print 2
+    number = number / 10;  //make number 2
+
+    int digit3 = (number % 10); // print 1
+    number = number / 10;  //make number 0
+    System.out.println(digit1);
+    System.out.println(digit2);
+    System.out.println(digit3);
+  }
+}
 
